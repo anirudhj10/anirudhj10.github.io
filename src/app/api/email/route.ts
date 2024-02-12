188a1c8f9aa92efc99260c1eef68febff8c3c4f5
Anirudh Jagadish <anirudh101298@gmail.com>
2024-02-11 19:29:19 -0500

    added dependencies

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -8,7 +8,7 @@ const resend = new Resend(process.env.RESEND_KEY);
 
 export async function POST(req: Request) {
     const { name, email, message } = await req.json();
-    await resend.sendEmail({
+    (resend as any).sendEmail({
         from: 'Acme <onboarding@resend.dev>',
         to: 'nwalia.ez@gmail.com',
         subject: 'hello world',
